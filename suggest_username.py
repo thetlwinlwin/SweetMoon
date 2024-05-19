47bafbf924632d206ebfa5125bd0c31c3781b2ea
thetlwinlwin <minthantlwin8@gmail.com>
2024-05-19 15:26:02 +0630

    update code

--- suggest_username.py
+++ suggest_username.py
@@ -2,4 +2,11 @@ user_input = input("Enter some name in your mind : ")
 
 while len(user_input) < 4 or len(user_input) > 12:
     print("\n Invalid Input. Try Again\n")
+
+    print(f'\nTry like "cheeselover {user_input}"')
+    print(f'\nTry like "Moonlit Sun {user_input}"')
+    print(f'\nTry like "Hello Darkness my old friend {user_input}"\n')
+
     user_input = input("Enter some name in your mind : ")
+
+print("username is accepted")
