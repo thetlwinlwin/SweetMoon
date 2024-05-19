8b52af00ad215231ebd84483eb5813be92135663
thetlwinlwin <minthantlwin8@gmail.com>
2024-05-19 22:15:25 +0630

    another version of get

--- get_latest_login(asc).py
+++ get_latest_login(asc).py
@@ -1,22 +1,40 @@
 user_name = input("Enter username : ")
 
 textfile = open("users(asc).txt")
-textfile.close()
 
-lines = textfile.readlines()
+# lines = textfile.readlines()
 
-user_index = 0
+# # user_index = 0
+# # user_found = False
+
+# # for i in range(len(lines)):
+# #     user = lines[i].strip()
+# #     if user == user_name:
+# #         user_index = i
+# #         user_found = True
+
+# # latest_login = lines[user_index + 1]
+# # print(f"latest login at {latest_login}")
+
+# # if not user_found:
+# #     print("User not Found")
+
+
 user_found = False
 
-for i in range(len(lines)):
-    user = lines[i].strip()
+while not user_found:
+    user_logins = []
+    user = textfile.readline().strip()
+    if user == "":
+        break
     if user == user_name:
-        user_index = i
-        user_found = True
+        login_date = textfile.readline().strip()
+        user_logins.append(login_date)
 
-latest_login = lines[user_index + 1]
-print(f"latest login at {latest_login}")
-
-
-if not user_found:
+if user_found:
+    print(f"latest login is {user_logins[-1]}")
+else:
     print("User not Found")
+
+
+textfile.close()
