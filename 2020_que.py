9e8ec13c1d0c7fcdcac51fc5d9c604f76c59d898
thetlwinlwin <minthantlwin8@gmail.com>
2024-07-10 15:48:39 +0630

    task 1 done

--- 2020_que.py
+++ 2020_que.py
@@ -1,9 +1,10 @@
 print("Hello WElCOMe from car paradise\n")
 
 CAR_PRICE = 0
-CAR_DISCOUNT = 0
+CAR_DISCOUNT_PERCENT = 0
+TRADE_IN_VALUE = 0
 
-user_choice = input("cars => saloon(s), hatch(h) or estate(e) : ")
+user_choice = input("cars => saloon(s), hatch(h) or estate(e) : ").lower()
 
 if user_choice == "s":
     CAR_PRICE = 495000
@@ -17,12 +18,12 @@ else:
 
 
 EXTRA_PRICE = 0
-EXTRA_DISCOUNT = 0
+EXTRA_DISCOUNT_PRECENT = 0
 
-extras = input("Do you want to add any extras? (y/n) : ")
+extras = input("Do you want to add any extras? (y/n) : ").lower()
 while extras == "y":
     extra = input(
-        "(ls) Set of luxury seats or (n) Satellite navigation or (p) Parking sensors or (b) Bluetooth connectivity or (s) Sound system : "
+        "(ls) luxury seats or (n) navigation or (p) parking or (b) bluetooth or (s) sound system : "
     ).lower()
     if extra == "ls":
         EXTRA_PRICE += 45000
@@ -37,4 +38,26 @@ while extras == "y":
     else:
         print("Invalid input")
         exit()
-    extras = input("Do you want to add any more extras? (y/n) : ")
+    extras = input("Do you want to add any more extras? (y/n) : ").lower()
+
+
+is_old_customer = input("Are you old customer y/n : ").lower()
+
+if is_old_customer == "y":
+    CAR_DISCOUNT_PERCENT += 0.1
+    EXTRA_DISCOUNT_PRECENT += 0.1
+
+is_trade_in = input("Do you want to trade in your car y/n : ").lower()
+
+if is_trade_in == "y":
+    CAR_DISCOUNT_PERCENT += 0.05
+    EXTRA_DISCOUNT_PRECENT += 0.05
+    offer_amount = int(input("We the offer : "))
+    TRADE_IN_VALUE = offer_amount
+
+AFTER_CAR_PRICE = CAR_PRICE - (CAR_PRICE * CAR_DISCOUNT_PERCENT) - TRADE_IN_VALUE
+AFTER_EXTRA_PRICE = EXTRA_PRICE - (EXTRA_PRICE * EXTRA_DISCOUNT_PRECENT)
+TOTAL_PRICE = AFTER_CAR_PRICE + AFTER_EXTRA_PRICE
+
+
+print(f"Total price is {TOTAL_PRICE}")
